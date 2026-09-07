864e7fdd6903a800216f66432263719a1aa159cf
spiraletech <creeshchris@gmail.com>
2026-09-06 22:06:11 -0700

    L4: expose topology through versioned world

--- include/home/versioned_world.hpp
+++ include/home/versioned_world.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "home/entity_registry.hpp"
+#include "home/topology.hpp"
 #include "home/world_delta.hpp"
 
 #include <cstddef>
+#include <optional>
 #include <vector>
 
 namespace home {
@@ -15,6 +17,7 @@ public:
     [[nodiscard]] WorldId world() const noexcept { return registry_.world(); }
     [[nodiscard]] WorldRevision revision() const noexcept { return revision_; }
     [[nodiscard]] const EntityRegistry& entities() const noexcept { return registry_; }
+    [[nodiscard]] const TopologyRegistry& topology() const noexcept { return topology_; }
     [[nodiscard]] const std::vector<WorldDelta>& history() const noexcept { return history_; }
 
     Result<EntityId> create_entity(EntityCreateInfo info);
@@ -22,12 +25,20 @@ public:
     Result<EntityRecord> remove_entity(EntityId id);
     Result<void> update_transform(EntityId id, Transform transform);
 
+    Result<ZoneId> create_zone(ZoneCreateInfo info);
+    Result<void> restore_zone(ZoneRecord zone);
+    Result<void> set_zone_parent(ZoneId child, std::optional<ZoneId> parent);
+    Result<void> connect_zones(ZoneConnection connection);
+    Result<void> place_entity(EntityId entity, ZoneId zone);
+    Result<void> clear_entity_zone(EntityId entity);
+
     [[nodiscard]] Result<std::vector<WorldDelta>> deltas_since(WorldRevision revision) const;
 
 private:
     Result<void> commit(std::vector<WorldChange> changes);
 
     EntityRegistry registry_;
+    TopologyRegistry topology_;
     WorldRevision revision_{};
     std::vector<WorldDelta> history_{};
 };
