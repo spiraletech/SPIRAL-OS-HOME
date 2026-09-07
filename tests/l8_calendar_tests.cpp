9dff231bc102349d1c76777992d4baf2b5c5fffa
spiraletech <creeshchris@gmail.com>
2026-09-06 22:32:26 -0700

    L8: avoid assert macro comma parsing

--- tests/l8_calendar_tests.cpp
+++ tests/l8_calendar_tests.cpp
@@ -5,21 +5,25 @@
 
 int main() {
     using namespace home;
+    const CalendarDate jan1{2026, 1, 1};
+    const CalendarDate jan2{2026, 1, 2};
+    const CalendarDate mar1{2026, 3, 1};
+
     const auto epoch = resolve_calendar(WorldTime{0});
-    assert(epoch.date == CalendarDate{2026, 1, 1});
+    assert(epoch.date == jan1);
     assert(epoch.hour == 0 && epoch.minute == 0 && epoch.second == 0);
     assert(epoch.weekday == 4);
     assert(epoch.season == Season::Winter);
     const auto day_two = resolve_calendar(WorldTime{86400000});
-    assert(day_two.date == CalendarDate{2026, 1, 2});
+    assert(day_two.date == jan2);
     assert(day_two.day_index == 1);
     const auto spring = resolve_calendar(WorldTime{59ULL * 86400000ULL});
-    assert(spring.date == CalendarDate{2026, 3, 1});
+    assert(spring.date == mar1);
     assert(spring.season == Season::Spring);
     VersionedWorld world{WorldId{9}};
     const auto advance = world.advance_time(43200000);
     assert(advance.ok());
-    assert(world.calendar().date == CalendarDate{2026, 1, 2});
+    assert(world.calendar().date == jan2);
     assert(world.calendar().hour == 0);
     assert(std::string_view{season_name(Season::Summer)} == "summer");
     assert(std::string_view{season_name(Season::Autumn)} == "autumn");
