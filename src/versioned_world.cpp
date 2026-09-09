2f07a418e9895a98c1fd2d878f9c260b6df06fcb
spiraletech <creeshchris@gmail.com>
2026-09-08 18:10:52 -0700

    L16: fix world deletion compile declaration

--- src/versioned_world.cpp
+++ src/versioned_world.cpp
@@ -104,8 +104,6 @@ Result<void> VersionedWorld::update_transform(EntityId id, Transform transform) 
     if(!c){r->transform=before;return c;} return Result<void>::success();
 }
 
-Result<ZoneId> VersionedWorld::create_zone(EntityCreateInfo) = delete;
-
 Result<ZoneId> VersionedWorld::create_zone(ZoneCreateInfo info) {
     const auto z=topology_.create_zone(std::move(info)); if(!z)return z; const auto* r=topology_.find(z.value());
     if(!r)return Result<ZoneId>::failure(ErrorCode::InternalError,"created zone missing from topology");
